78c9c5ce763ace283c360ed5778e338332781263
root <root@DESKTOP-58N03D2.localdomain>
2019-12-30 18:48:41 -0600

    create openstack branch

--- lib/ConnectionUtilities.py
+++ lib/ConnectionUtilities.py
@@ -1,5 +1,13 @@
-import libvirt
+import openstack
 
-def openConnection(host):
-    conn = libvirt.open(host)
-    return conn
+def create_connection(auth_url, region, project_name, username, password):
+
+    return openstack.connect(
+        auth_url=auth_url,
+        project_name=project_name,
+        username=username,
+        password=password,
+        region_name=region,
+        app_name='examples',
+        app_version='1.0',
+    )
