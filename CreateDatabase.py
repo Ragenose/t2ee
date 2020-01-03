c443c438dccc0bc05f7d6fa6fb4987c9e0da8d22
Ragenose <zuoxiu.xing@gmail.com>
2020-01-02 22:25:44 -0600

    Script to generate initial database

--- CreateDatabase.py
+++ CreateDatabase.py
@@ -3,6 +3,17 @@ import pymongo
 import yaml
 
 with open("config/database.yaml", 'r') as stream:
-            data_loaded = yaml.safe_load(stream) 
+    data_loaded = yaml.safe_load(stream) 
 
 url = data_loaded["url"]
+
+client = pymongo.MongoClient(url)
+
+db = client["t2ee"]
+
+openstack_col = db["openstack"]
+
+with open("config/compute.yaml", 'r') as stream:
+    data_loaded = yaml.safe_load(stream) 
+
+openstack_col.insert_one(data_loaded)
