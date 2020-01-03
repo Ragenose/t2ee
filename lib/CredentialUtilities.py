83352edbe640f4c4ecf70b045c053e95d4516cb3
Ragenose <zuoxiu.xing@gmail.com>
2020-01-02 18:43:46 -0600

    Added create user function

---
+++ lib/CredentialUtilities.py
@@ -0,0 +1,20 @@
+import openstack
+
+'''
+Function: create_user
+Date: 2020/01/02
+Purpose: Create user
+Parameters: 
+    conn: OpenStack connection
+    name: User name
+    password: User password
+    email: User email
+Return value: 
+    instance: openstack.compute.v2.server.Server object
+'''
+def create_user(conn, name, password, email):
+    return conn.identity.create_user(
+        name = name,
+        password = password,
+        email = email
+    )
