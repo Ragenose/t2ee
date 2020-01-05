18d5fdb60ea5d6d5d9bc1a215eecaf805e5805ac
Ragenose <zuoxiu.xing@gmail.com>
2020-01-05 02:48:35 +0000

    Updated files

--- api/main.py
+++ api/main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 from lib.ConnectionUtilities import create_connection_from_config
+from lib.InstanceUtilities import create_instance
 
 conn = create_connection_from_config()
+create_instance(conn, "Ubuntu16.04", "small", "provider1", "test_docker")
