9f71ff6b150e67504e74c060bb69099adcc1e7ad
Ragenose <zuoxiu.xing@gmail.com>
2020-01-05 03:05:51 +0000

    Added mongoDB service

--- api/main.py
+++ api/main.py
@@ -3,4 +3,3 @@ from lib.ConnectionUtilities import create_connection_from_config
 from lib.InstanceUtilities import create_instance
 
 conn = create_connection_from_config()
-create_instance(conn, "Ubuntu16.04", "small", "provider1", "test_docker")
