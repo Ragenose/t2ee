26ad419bd1a9e893018f36ef3560b796d4b74913
Ragenose <zuoxiu.xing@gmail.com>
2020-01-06 03:28:01 +0000

    Added update user attributes function

--- api/lib/CredentialUtilities.py
+++ api/lib/CredentialUtilities.py
@@ -1,4 +1,4 @@
-from lib.DatabaseUtilities import create_user_document
+from lib.DatabaseUtilities import create_db_connection, create_user_document
 
 # Function: check_user_available
 # Date: 2020/01/04
@@ -12,6 +12,25 @@ from lib.DatabaseUtilities import create_user_document
 def check_user_available(conn, name):
     user = conn.identity.find_user(name)
     if(user is None):
+        return True
+    else:
+        return False
+
+# Function: check_email_available
+# Date: 2020/01/04
+# Purpose: Check if the email is taken
+# Parameters:
+#     conn: OpenStack connection
+#     name: The user name that needs to be checked
+# Return value:
+#     True: If it is not taken
+#     False: If it is taken
+def check_email_available(conn, email):
+    client = create_db_connection()
+    db = client["t2ee"]
+    user_col = db["user"]
+    result = user_col.find({"email": email})
+    if(result.retrieved == 0):
         return True
     else:
         return False
@@ -40,3 +59,61 @@ def create_user(conn, name, password, email):
     else:
         create_user_document(user, password)
         return user
+
+# Function: update_user_email
+# Date: 2020/01/05
+# Purpose: Update user email
+# Parameters:
+#     conn: OpenStack connection
+#     name: User name
+#     email: User's new email
+# Return value:
+#     False: If update failed
+#     True: If update succeed
+def update_user_email(conn, name, email):
+    try:
+        user = conn.identity.find_user(name)
+        user.update_user(
+            user,
+            email = email
+        )
+        client = create_db_connection()
+        db = client["t2ee"]
+        user_col = db["user"]
+        query = {"name": name}
+        new_email = {"$set": {"email": email}}
+        user_col.update_one(query, new_email)
+
+    except Exception:
+        return False
+    else:
+        return True
+
+# Function: update_user_password
+# Date: 2020/01/05
+# Purpose: Update user email
+# Parameters:
+#     conn: OpenStack connection
+#     name: User name
+#     email: User's new email
+# Return value:
+#     False: If update failed
+#     True: If update succeed
+def update_user_password(conn, name, password):
+    try:
+        user = conn.identity.find_user(name)
+        user.update_user(
+            user,
+            password = password
+        )
+        client = create_db_connection()
+        db = client["t2ee"]
+        user_col = db["user"]
+        query = {"name": name}
+        new_password = {"$set": {"password": password}}
+        user_col.update_one(query, new_password)
+
+    except Exception:
+        return False
+    else:
+        return True
