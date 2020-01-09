8461cd1567a6f3f0fe056a368a3ed01f49ed31cd
Ragenose <zuoxiu.xing@gmail.com>
2020-01-09 03:05:39 +0000

    Added unit test for adding instance under user

--- api/test/test_database.py
+++ api/test/test_database.py
@@ -3,13 +3,21 @@ import unittest
 
 from lib.ConnectionUtilities import create_connection_from_config
 from lib.CredentialUtilities import create_user
-from lib.DatabaseUtilities import create_db_connection, get_network_name
+from lib.DatabaseUtilities import create_db_connection, get_network_name, add_instance_to_user
+
 conn = create_connection_from_config()
+client = create_db_connection()
+db = client["t2ee"]
 
 class test_database(unittest.TestCase):
     def test_get_network_name(self):
-        
         self.assertEqual(get_network_name(), "provider1")
+    
+    def test_add_instance_to_user(self):
+        add_instance_to_user("test_user1", "test_instance")
+        user_col = db["user"]
+        result = user_col.find_one({"name": "test_user1", "instance": {"$elemMatch":{"instance_name":"test_instance"}}})
+        self.assertIsNotNone(result)
 
 if __name__ == '__main__':
     unittest.main()
