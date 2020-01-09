02841206dd49882c0ffc99b6e739b407e454fde8
Ragenose <zuoxiu.xing@gmail.com>
2020-01-09 03:32:29 +0000

    Added function to create instance from message

--- api/callback/Instance.py
+++ api/callback/Instance.py
@@ -1,8 +1,16 @@
 import pika
 import json
 from lib.ConnectionUtilities import create_connection_from_config
-from lib.InstanceUtilities import create_instance
-from lib.DatabaseUtilities import create_db_connection
+from lib.InstanceUtilities import create_instance, check_instance_name_available
+from lib.DatabaseUtilities import create_db_connection, get_network_name, add_instance_to_user
+
+# Function: mq_instance
+# Date: 2020/01/08
+# Purpose: Multiplexing instance requests from message
+# Parameters:
+#     ch, method, properties, body: RabbitMQ parameters
+# Return value:
+#     instance: openstack.compute.v2.server.Server object
 
 def mq_instance(ch, method, properties, body):
     payload = json.loads(body.decode("utf-8"))
@@ -13,7 +21,16 @@ def mq_instance(ch, method, properties, body):
         print("KeyError")
     
 
+# Function: mq_instance
+# Date: 2020/01/08
+# Purpose: Multiplexing instance requests from message
+# Parameters:
+#     ch, method, properties, body: RabbitMQ parameters
+# Return value:
+#     instance: openstack.compute.v2.server.Server object
 
 def mq_create_instance(username, instance_name, image, flavor):
     conn = create_connection_from_config()
-    create_instance(conn, image, flavor, "provider1", instance_name)
+    if(check_instance_name_available(conn, instance_name) is True):
+        create_instance(conn, image, flavor, get_network_name(), instance_name)
+        add_instance_to_user(username, instance_name)
