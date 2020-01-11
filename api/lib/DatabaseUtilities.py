8615d1cabfa12dad8989c56d8f9d23cabd2b2759
Ragenose <zuoxiu.xing@gmail.com>
2020-01-11 06:29:39 +0000

    Added image related function

--- api/lib/DatabaseUtilities.py
+++ api/lib/DatabaseUtilities.py
@@ -1,6 +1,6 @@
 import yaml
 import pymongo
-
+from lib.ConnectionUtilities import create_connection_from_config
 
 # Function: create_db_connection
 # Date: 2020/01/02
@@ -16,7 +16,6 @@ def create_db_connection():
     url = data_loaded["url"]
     client = pymongo.MongoClient(url)
     return client
-
 
 # Function: create_user_document
 # Date: 2020/01/02
@@ -42,6 +41,38 @@ def create_user_document(user, password):
     user_col.insert_one(user_data)
     client.close()
 
+# Function: create_image_document
+# Date: 2020/01/02
+# Purpose: create image document in mongodb
+# Parameters:
+#     user: OpenStack user object
+#     password: User's password
+# Return value:
+#     None
+
+def create_image_document(name, image_name, instance_name):
+    client = create_db_connection()
+    db = client["t2ee"]
+    image_col = db["image"]
+    conn = create_connection_from_config()
+    
+    instance = conn.compute.find_server(instance_name)
+    try:
+        image_id = conn.compute.get_server(instance.id).image.id
+    except:
+        return
+    else:
+        base_image_name = conn.image.find_image(image_id).name
+        print(base_image_name)
+        image_data = {
+            "username" : name,
+            "image_id": image_id,
+            "image_name": image_name,
+            "base_image_name": base_image_name
+        }
+        image_col.insert_one(image_data)
+    client.close()
+
 # Function: get_network_name
 # Date: 2020/01/08
 # Purpose: Get network name from database
@@ -62,16 +93,18 @@ def get_network_name():
 # Date: 2020/01/08
 # Purpose: Add instance under user's name
 # Parameters:
-#     None
+#     user: User's name
+#     instance_name: Instance name
+#     instance_id: Instance id
 # Return value:
 #     None
 
-def add_instance_to_user(user, instance_name):
+def add_instance_to_user(user, instance_name, instance_id):
     client = create_db_connection()
     user_col = client["t2ee"]["user"]
     user_col.update(
         {"name": user},
-        {"$push": {"instance": {"instance_name": instance_name}}}
+        {"$push": {"instance": {"instance_name": instance_name, "instance_id": instance_id}}}
     )
     client.close()
 
@@ -79,7 +112,8 @@ def add_instance_to_user(user, instance_name):
 # Date: 2020/01/09
 # Purpose: Remove instance under user's name
 # Parameters:
-#     None
+#     user: User's name
+#     instance_name: Instance name
 # Return value:
 #     None
 
@@ -89,5 +123,42 @@ def remove_instance_from_user(user, instance_name):
     user_col.update(
         {"name": user},
         {"$pull": {"instance": {"instance_name": instance_name}}}
+    )
+    client.close()
+
+# Function: add_image_to_user
+# Date: 2020/01/10
+# Purpose: Add image under user's name
+# Parameters:
+#     user: User's name
+#     image_name: Image name
+#     image_id: Image id
+# Return value:
+#     None
+
+def add_image_to_user(user, image_name, image_id):
+    client = create_db_connection()
+    user_col = client["t2ee"]["user"]
+    user_col.update(
+        {"name": user},
+        {"$push": {"image": {"image_name": image_name, "image_id": image_id}}}
+    )
+    client.close()
+
+# Function: remove_image_from_user
+# Date: 2020/01/09
+# Purpose: Remove image under user's name
+# Parameters:
+#     user: User's name
+#     image_name: Image name
+# Return value:
+#     None
+
+def remove_image_from_user(user, image_name):
+    client = create_db_connection()
+    user_col = client["t2ee"]["user"]
+    user_col.update(
+        {"name": user},
+        {"$pull": {"image": {"image_name": image_name}}}
     )
     client.close()
