bc727225ee8fc4b45064b0a70e8473fcb127e5f2
Ragenose <zuoxiu.xing@gmail.com>
2020-01-11 06:48:12 +0000

    Added MQ create image function

--- api/mq.py
+++ api/mq.py
@@ -1,6 +1,7 @@
 import pika
 import json
 from callback.Instance import mq_instance
+from callback.Image import mq_image
 
 credentials = pika.PlainCredentials('rabbit','rabbit')
 connection = pika.BlockingConnection(pika.ConnectionParameters(
@@ -12,5 +13,6 @@ channel.queue_declare(queue='image',durable=True)
 
 
 channel.basic_consume(on_message_callback=mq_instance,queue='instance', auto_ack=True)
+channel.basic_consume(on_message_callback=mq_image, queue='image', auto_ack=True)
 
 channel.start_consuming()                          
