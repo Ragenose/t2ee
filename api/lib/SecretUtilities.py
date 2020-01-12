15d02b51feb997a977702cee1230abdad2309aa1
Ragenose <zuoxiu.xing@gmail.com>
2020-01-12 06:10:42 +0000

    Rename to Secret

--- api/lib/SecretUtilities.py
+++ api/lib/SecretUtilities.py
@@ -1,10 +1,13 @@
 import pymongo
 from lib.ConnectionUtilities import create_connection_from_config
+from lib.DatabaseUtilities import add_keypair_to_user
 
 def create_keypair(user, pubkey):
     conn = create_connection_from_config()
     conn.compute.create_keypair(
-        name = user,
+        name = user+"_key",
         public_key = pubkey
     )
+    add_keypair_to_user(user, user+"_key")
+    conn.close()
     
