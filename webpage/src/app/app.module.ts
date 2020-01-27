f2022b98972ec63b1368ef73b10de8c9084f0bef
Ragenose <zuoxiu.xing@gmail.com>
2020-01-27 04:53:50 +0000

    Moved navi bar to app root

--- webpage/src/app/app.module.ts
+++ webpage/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -15,7 +14,6 @@ import { ErrorInterceptor } from './helpers/error.interceptor';
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
     LoginComponent
   ],
   imports: [
