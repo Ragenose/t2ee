9d4e5eb338a8c5e07934e7bf03ee15473584db9d
Ragenose <zuoxiu.xing@gmail.com>
2020-02-10 05:33:58 +0000

    Added retry set root password

--- api/callback/Instance.py
+++ api/callback/Instance.py
@@ -2,6 +2,7 @@ import pika
 import json
 import logging
 import time
+import openstack
 from lib.ConnectionUtilities import create_connection_from_config
 from lib.InstanceUtilities import\
     create_instance,\
@@ -51,11 +52,19 @@ def mq_create_instance(username, instance_name, image, flavor, root_password):
     conn = create_connection_from_config()
     if(check_instance_name_available(conn, instance_name) is True):
         keypair = get_keypair(username)
-        # root_password = get_root_password(username)
         instance = create_instance(conn, image, flavor, get_network_name(), instance_name, root_password=root_password, keypair=keypair)
         add_instance_to_user(username, instance_name, instance.id)
-        time.sleep(40)
-        conn.compute.change_server_password(instance, root_password)
+        # Wait for 30 second to let instance boot up
+        time.sleep(30)
+        # Try to set the root password if it fails
+        for i in range(0,10):
+            while True:
+                try:
+                    conn.compute.change_server_password(instance, root_password)
+                except openstack.exceptions.HttpException:
+                    continue
+            break
+        
     conn.close()
 
 # Function: mq_delete_instance
