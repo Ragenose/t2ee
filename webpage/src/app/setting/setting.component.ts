ee5136e6fd746b62aef37484b8d0bcc94e207e8c
Ragenose <zuoxiu.xing@gmail.com>
2020-02-17 01:29:58 +0000

    Added update pubkey function

--- webpage/src/app/setting/setting.component.ts
+++ webpage/src/app/setting/setting.component.ts
@@ -1,3 +1,6 @@
+import { AuthenticationService } from './../services/authentication.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,8 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./setting.component.css']
 })
 export class SettingComponent implements OnInit {
+  pubkeyForm: FormGroup;
+  passwordForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuild: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) { }
 
   ngOnInit() {
   }
