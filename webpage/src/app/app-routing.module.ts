69d3d389e1ccdf5382c8791b980763ee33b68b3c
Ragenose <zuoxiu.xing@gmail.com>
2020-02-19 23:18:24 +0000

    Added new routing path

--- webpage/src/app/app-routing.module.ts
+++ webpage/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './helpers/auth.guard';
 import { HomeComponent } from './home/home.component';
+import { ImageComponent } from './image/image.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent, canActivate: [AuthGuard]},
   {path: 'deploy', component: DeployComponent, canActivate: [AuthGuard]},
   {path: 'login', component: LoginComponent},
-  {path: 'setting', component: SettingComponent}
+  {path: 'setting', component: SettingComponent},
+  {path: 'image', component: ImageComponent}
 ];
 
 @NgModule({
