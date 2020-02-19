5479df16785cd378a53a6c7cb979e7ffea1b1c55
Ragenose <zuoxiu.xing@gmail.com>
2020-02-19 23:18:14 +0000

    Added get image list function

---
+++ webpage/src/app/services/image.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageService {
+
+  constructor(private http: HttpClient) { }
+
+  getImageList(){
+    return this.http.get("/api/image/list");
+  }
+}
