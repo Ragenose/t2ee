7fc5f26ed03f008330989f8bf5dde21f79f084a8
Ragenose <zuoxiu.xing@gmail.com>
2020-03-01 06:46:09 +0000

    Added displaying username in setting menu

--- webpage/src/app/app.component.ts
+++ webpage/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { AuthenticationService } from './services/authentication.service';
 import { Component } from '@angular/core';
-import {DomSanitizer} from '@angular/platform-browser';
+import { DomSanitizer } from '@angular/platform-browser';
 import { MatIconRegistry } from '@angular/material/icon';
 import { Router } from '@angular/router';
 
@@ -10,17 +10,30 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isLoggedin: boolean;
+  username: string;
+
   constructor(
-    iconRegistry: MatIconRegistry, 
+    iconRegistry: MatIconRegistry,
     sanitizer: DomSanitizer,
     private authenticationService: AuthenticationService,
     private router: Router) {
+    router.events.subscribe((val) => {
+        if(!JSON.parse(localStorage.getItem('currentUser'))){
+          this.isLoggedin = false;
+        }
+        else{
+          this.isLoggedin = true;
+          this.username = JSON.parse(localStorage.getItem('currentUser'))["username"];
+        } 
+    });
+    
     iconRegistry.addSvgIcon(
-        'account',
-        sanitizer.bypassSecurityTrustResourceUrl('assets/img/account_circle-24px.svg'));
+      'account',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/account_circle-24px.svg'));
   }
 
-  logout(){
+  logout() {
     this.authenticationService.logout();
     this.router.navigate(['/login']);
   }
